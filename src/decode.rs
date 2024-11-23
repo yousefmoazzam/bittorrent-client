3e204d9bed261bb2740b4e3a5178e073e6dd4d9d
Yousef Moazzam <yousefmoazzam@hotmail.co.uk>
2024-11-23 22:49:51 +0000

    Decode a bencoded byte string

--- src/decode.rs
+++ src/decode.rs
@@ -4,6 +4,15 @@ use regex::Regex;
 
 /// Decode bencoded data
 pub fn decode(data: &str) -> BencodeType {
+    let byte_str_len_regex = Regex::new(r"\d+:").unwrap();
+    let res = byte_str_len_regex.find(data);
+    if let Some(mat) = res {
+        let str_len = mat.as_str()[..mat.len() - 1].parse::<usize>().unwrap();
+        // Subtract 1 for the colon character
+        let no_of_digits_in_str_len = mat.len() - 1;
+        return decode_byte_string(data, str_len, no_of_digits_in_str_len);
+    }
+
     let type_char = data.chars().next().unwrap();
     match type_char {
         'i' => decode_integer(&data[..]),
@@ -21,6 +30,14 @@ fn decode_integer(data: &str) -> BencodeType {
     };
     let integer = value.as_str()[1..value.len() - 1].parse::<i64>().unwrap();
     BencodeType::Integer(integer)
+}
+
+/// Decode a bencoded byte string
+fn decode_byte_string(data: &str, str_len: usize, no_of_digits_in_len: usize) -> BencodeType {
+    let start = no_of_digits_in_len + 1;
+    let stop = start + str_len;
+    let string = &data[start..stop];
+    BencodeType::ByteString(string.to_string())
 }
 
 #[cfg(test)]
@@ -42,6 +59,15 @@ mod tests {
         let bencoded_data = format!("i{}e", integer);
         let expected_output = BencodeType::Integer(integer);
         let output = decode(&bencoded_data[..]);
+        assert_eq!(output, expected_output);
+    }
+
+    #[test]
+    fn decode_byte_string() {
+        let string = "hello";
+        let bencoded_data = format!("{}:{}", string.len(), string);
+        let expected_output = BencodeType::ByteString(string.to_string());
+        let output = decode(&bencoded_data);
         assert_eq!(output, expected_output);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,4 +3,5 @@ pub mod decode;
 #[derive(Debug, PartialEq)]
 pub enum BencodeType {
     Integer(i64),
+    ByteString(String),
 }
