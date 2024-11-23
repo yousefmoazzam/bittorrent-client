3a09be58bbad813ac28e287e4f40f50caae148b4
Yousef Moazzam <yousefmoazzam@hotmail.co.uk>
2024-11-23 22:19:27 +0000

    Decode a bencoded positive integer

---
+++ src/decode.rs
@@ -0,0 +1,38 @@
+use crate::BencodeType;
+
+use regex::Regex;
+
+/// Decode bencoded data
+pub fn decode(data: &str) -> BencodeType {
+    let type_char = data.chars().next().unwrap();
+    match type_char {
+        'i' => decode_integer(&data[..]),
+        _ => todo!(),
+    }
+}
+
+/// Decode a bencoded integer
+fn decode_integer(data: &str) -> BencodeType {
+    let regex = Regex::new(r"i\d+e").unwrap();
+    let res = regex.find(data);
+    let value = match res {
+        Some(mat) => mat,
+        None => todo!(),
+    };
+    let integer = value.as_str()[1..value.len() - 1].parse::<i64>().unwrap();
+    BencodeType::Integer(integer)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn decode_positive_integer() {
+        let integer = 42;
+        let bencoded_data = format!("i{}e", integer);
+        let expected_output = BencodeType::Integer(integer);
+        let output = decode(&bencoded_data[..]);
+        assert_eq!(output, expected_output);
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+pub mod decode;
+
+#[derive(Debug, PartialEq)]
+pub enum BencodeType {
+    Integer(i64),
+}
