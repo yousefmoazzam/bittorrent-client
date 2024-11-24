41377cbe6cecb1d233b5522608580abad89281b8
Yousef Moazzam <yousefmoazzam@hotmail.co.uk>
2024-11-24 22:10:33 +0000

    Return error if info dict missing `pieces` key

--- src/metainfo.rs
+++ src/metainfo.rs
@@ -58,6 +58,7 @@ impl Metainfo {
 const NAME_KEY: &str = "name";
 const LENGTH_KEY: &str = "length";
 const PIECE_LENGTH_KEY: &str = "piece length";
+const PIECES_KEY: &str = "pieces";
 
 /// Info dict within metainfo file
 struct Info;
@@ -66,7 +67,7 @@ impl Info {
     fn new(data: BencodeType) -> Result<(), String> {
         match data {
             BencodeType::Dict(dict) => {
-                for key in [NAME_KEY, LENGTH_KEY, PIECE_LENGTH_KEY] {
+                for key in [NAME_KEY, LENGTH_KEY, PIECE_LENGTH_KEY, PIECES_KEY] {
                     if !dict.contains_key(key) {
                         return Err(format!(
                             "Invalid info dict, the following key is missing: {}",
@@ -175,6 +176,21 @@ mod tests {
         map.insert("length".to_string(), BencodeType::Integer(64));
         let data = BencodeType::Dict(map);
         let expected_err_msg = "Invalid info dict, the following key is missing: piece length";
+        let res = Info::new(data);
+        assert_eq!(true, res.is_err_and(|msg| msg == expected_err_msg));
+    }
+
+    #[test]
+    fn return_error_if_info_dict_missing_pieces_key() {
+        let mut map = HashMap::new();
+        map.insert(
+            "name".to_string(),
+            BencodeType::ByteString("hello".to_string()),
+        );
+        map.insert("length".to_string(), BencodeType::Integer(64));
+        map.insert("piece length".to_string(), BencodeType::Integer(256));
+        let data = BencodeType::Dict(map);
+        let expected_err_msg = "Invalid info dict, the following key is missing: pieces";
         let res = Info::new(data);
         assert_eq!(true, res.is_err_and(|msg| msg == expected_err_msg));
     }
