e4bb6fe91d96b5d352d699d2891d2d10c18df7b9
Yousef Moazzam <yousefmoazzam@hotmail.co.uk>
2024-11-27 21:32:26 +0000

    Deserialise data into handshake struct

--- src/handshake.rs
+++ src/handshake.rs
@@ -1,6 +1,9 @@
 const PROTOCOL_ID_LEN: u8 = 0x13;
 const PSTR: &str = "BitTorrent protocol";
 const PEER_ID: &str = "-ABC123-abcd12345678";
+const NO_OF_RESERVED_BYTES: u8 = 8;
+const INFO_HASH_LEN_BYTES: u8 = 20;
+const PEER_ID_LEN_BYTES: u8 = 20;
 
 /// BitTorrent handshake
 pub struct Handshake {
@@ -18,6 +21,30 @@ impl Handshake {
             pstr,
             info_hash,
             peer_id,
+        }
+    }
+
+    /// Deserialise data into handshake
+    pub fn deserialise(data: &[u8]) -> Handshake {
+        let protocol_identifier_len = data[0];
+        let protocol_identifier = &data[1..(1 + protocol_identifier_len) as usize];
+        let _reserved_bytes = &data[(1 + protocol_identifier_len) as usize
+            ..(1 + protocol_identifier_len + NO_OF_RESERVED_BYTES) as usize];
+        let info_hash = &data[(1 + protocol_identifier_len + NO_OF_RESERVED_BYTES) as usize
+            ..(1 + protocol_identifier_len + NO_OF_RESERVED_BYTES + INFO_HASH_LEN_BYTES) as usize];
+        let peer_id =
+            &data[(1 + protocol_identifier_len + NO_OF_RESERVED_BYTES + INFO_HASH_LEN_BYTES)
+                as usize
+                ..(1 + protocol_identifier_len
+                    + NO_OF_RESERVED_BYTES
+                    + INFO_HASH_LEN_BYTES
+                    + PEER_ID_LEN_BYTES) as usize];
+        Handshake {
+            pstr: std::str::from_utf8(protocol_identifier)
+                .unwrap()
+                .to_string(),
+            info_hash: info_hash.to_vec(),
+            peer_id: peer_id.to_vec(),
         }
     }
 
@@ -50,5 +77,19 @@ mod tests {
             PEER_ID.to_string().as_bytes().to_vec(),
         );
         assert_eq!(handshake.serialise(), expected);
+    }
+
+    #[test]
+    fn deserialised_handshake_is_correct() {
+        let info_hash = (0x00..0x14).collect::<Vec<_>>();
+        let mut buffer = vec![PROTOCOL_ID_LEN];
+        buffer.append(&mut PSTR.as_bytes().to_vec());
+        buffer.append(&mut vec![0x00; 8]);
+        buffer.append(&mut info_hash.clone());
+        buffer.append(&mut PEER_ID.as_bytes().to_vec());
+        let handshake = Handshake::deserialise(&buffer[..]);
+        assert_eq!(PSTR.to_string(), handshake.pstr);
+        assert_eq!(PEER_ID.to_string().as_bytes().to_vec(), handshake.peer_id);
+        assert_eq!(info_hash, handshake.info_hash);
     }
 }
