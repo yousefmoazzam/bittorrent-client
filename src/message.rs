28612d47058963e39ca4373888f06ca40c888a10
Yousef Moazzam <yousefmoazzam@hotmail.co.uk>
2024-11-29 01:06:24 +0000

    Parse peer piece message

--- src/message.rs
+++ src/message.rs
@@ -17,6 +17,12 @@ pub enum Message {
         begin: u64,
         length: u64,
     },
+    /// Send a subset of a piece (a block)
+    Piece {
+        index: u64,
+        begin: u64,
+        block: Vec<u8>,
+    },
 }
 
 impl Message {
@@ -61,6 +67,21 @@ impl Message {
                     index,
                     begin,
                     length,
+                }
+            }
+            0x07 => {
+                let index = u64::from_be_bytes([
+                    bytes[0], bytes[1], bytes[2], bytes[3], bytes[4], bytes[5], bytes[6], bytes[7],
+                ]);
+                let begin = u64::from_be_bytes([
+                    bytes[8], bytes[9], bytes[10], bytes[11], bytes[12], bytes[13], bytes[14],
+                    bytes[15],
+                ]);
+                let block = bytes[16..].to_vec();
+                Message::Piece {
+                    index,
+                    begin,
+                    block,
                 }
             }
             _ => todo!(),
@@ -158,6 +179,27 @@ mod tests {
             index,
             begin,
             length,
+        };
+        let message = Message::new(&buf[..]);
+        assert_eq!(message, expected_message);
+    }
+
+    #[test]
+    fn parse_piece_message() {
+        let id = 0x07;
+        let index: u64 = 30;
+        let begin: u64 = 100;
+        let block = (0x00..0xFF).collect::<Vec<u8>>();
+        let len: u32 = 1 + 8 + 8 + block.len() as u32;
+        let mut buf = u32::to_be_bytes(len).to_vec();
+        buf.push(id);
+        buf.append(&mut u64::to_be_bytes(index).to_vec());
+        buf.append(&mut u64::to_be_bytes(begin).to_vec());
+        buf.append(&mut block.clone());
+        let expected_message = Message::Piece {
+            index,
+            begin,
+            block,
         };
         let message = Message::new(&buf[..]);
         assert_eq!(message, expected_message);
