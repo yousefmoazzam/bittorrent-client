24cc54e7efe25bd594a50a8f157e23e87eb2b6b9
Yousef Moazzam <yousefmoazzam@hotmail.co.uk>
2025-02-09 23:12:29 +0000

    Serialise dict to bencode format

--- src/serialise.rs
+++ src/serialise.rs
@@ -3,6 +3,7 @@ use crate::parse::BencodeType2;
 const COLON_ASCII: u8 = 58;
 const L_ASCII: u8 = 108;
 const E_ASCII: u8 = 101;
+const D_ASCII: u8 = 100;
 
 pub fn serialise(data: BencodeType2) -> Vec<u8> {
     match data {
@@ -24,7 +25,18 @@ pub fn serialise(data: BencodeType2) -> Vec<u8> {
             elems.push(E_ASCII);
             elems
         }
-        _ => todo!(),
+        BencodeType2::Dict(map) => {
+            let mut pairs = map.into_iter().collect::<Vec<(Vec<u8>, BencodeType2)>>();
+            pairs.sort_by_key(|pair| pair.0.clone());
+            let mut out = vec![D_ASCII];
+            for (key, val) in pairs.into_iter() {
+                let bencoded_key = BencodeType2::ByteString(key);
+                out.append(&mut serialise(bencoded_key));
+                out.append(&mut serialise(val));
+            }
+            out.push(E_ASCII);
+            out
+        }
     }
 }
 
@@ -32,6 +44,7 @@ pub fn serialise(data: BencodeType2) -> Vec<u8> {
 mod tests {
     use super::serialise;
     use crate::parse::BencodeType2;
+    use std::collections::HashMap;
 
     #[test]
     fn serialise_integer() {
@@ -60,6 +73,21 @@ mod tests {
             BencodeType2::Integer(integer),
         ]);
         let expected_output = b"l5:helloi42ee";
+        let output = serialise(data);
+        assert_eq!(output, expected_output);
+    }
+
+    #[test]
+    fn serialise_dict() {
+        let key1 = b"comment";
+        let value1 = b"Description of contents";
+        let key2 = b"creation date";
+        let value2 = 1234567890;
+        let mut map = HashMap::new();
+        map.insert(key1.to_vec(), BencodeType2::ByteString(value1.to_vec()));
+        map.insert(key2.to_vec(), BencodeType2::Integer(value2));
+        let data = BencodeType2::Dict(map);
+        let expected_output = b"d7:comment23:Description of contents13:creation datei1234567890ee";
         let output = serialise(data);
         assert_eq!(output, expected_output);
     }
