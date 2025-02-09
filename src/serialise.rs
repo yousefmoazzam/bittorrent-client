a038cdbe6d93d799d3e05aaf5d2a56a339e8c3b5
Yousef Moazzam <yousefmoazzam@hotmail.co.uk>
2025-02-09 22:36:43 +0000

    Serialise bytestring to bencode format

--- src/serialise.rs
+++ src/serialise.rs
@@ -1,10 +1,18 @@
 use crate::parse::BencodeType2;
 
+const COLON_ASCII: u8 = 58;
+
 pub fn serialise(data: BencodeType2) -> Vec<u8> {
     match data {
         BencodeType2::Integer(int) => {
             let string = format!("i{}e", int);
             string.as_bytes().to_vec()
+        }
+        BencodeType2::ByteString(mut string) => {
+            let mut out = string.len().to_string().as_bytes().to_owned();
+            out.push(COLON_ASCII);
+            out.append(&mut string);
+            out
         }
         _ => todo!(),
     }
@@ -20,6 +28,15 @@ mod tests {
         let integer = 42;
         let data = BencodeType2::Integer(integer);
         let expected_output = b"i42e".to_vec();
+        let output = serialise(data);
+        assert_eq!(output, expected_output);
+    }
+
+    #[test]
+    fn serialise_bytestring() {
+        let bytestring = b"hello";
+        let data = BencodeType2::ByteString(bytestring.to_vec());
+        let expected_output = b"5:hello".to_vec();
         let output = serialise(data);
         assert_eq!(output, expected_output);
     }
