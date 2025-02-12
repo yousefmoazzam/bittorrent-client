fcd320404e003ad51b14604f2a2012910795d22f
Yousef Moazzam <yousefmoazzam@hotmail.co.uk>
2025-02-12 20:50:00 +0000

    Log messages in binary crate rather than printing

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::{
     io::{Read, Write},
     path::PathBuf,
 };
+use tracing::info;
 
 #[derive(Parser)]
 struct Cli {
@@ -17,24 +18,28 @@ struct Cli {
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
+    tracing_subscriber::fmt().with_target(false).init();
     let cli = Cli::parse();
     if !cli.metainfo.exists() {
-        println!("File doesn't exist: {:?}", cli.metainfo);
-        println!("Exiting...");
+        info!("File doesn't exist: {:?}", cli.metainfo);
+        info!("Exiting...");
         std::process::exit(1);
     }
 
-    let mut file = File::open(cli.metainfo.clone()).expect(&format!(
-        "Expected to be able to open file: {:?}",
-        cli.metainfo
-    ));
+    let mut file = match File::open(cli.metainfo.clone()) {
+        Ok(val) => val,
+        Err(_) => {
+            info!("Expected to be able to open file: {:?}", cli.metainfo);
+            std::process::exit(1);
+        }
+    };
     let mut bytes = Vec::new();
     file.read_to_end(&mut bytes).unwrap();
 
     let bencoded_data = parse(&bytes);
     let metainfo = match Metainfo::new(bencoded_data) {
         Err(e) => {
-            println!("{}", e);
+            info!("{}", e);
             std::process::exit(1);
         }
         Ok(val) => val,
@@ -43,13 +48,13 @@ async fn main() {
     let data = download(torrent).await;
     let mut out_file = match File::create(cli.output.clone()) {
         Err(_) => {
-            println!("Unable to create output file: {:?}", cli.output);
+            info!("Unable to create output file: {:?}", cli.output);
             std::process::exit(1);
         }
         Ok(val) => val,
     };
     if out_file.write_all(&data).is_err() {
-        println!("Unable to save data to output file: {:?}", cli.output);
+        info!("Unable to save data to output file: {:?}", cli.output);
         std::process::exit(1);
     }
 }
